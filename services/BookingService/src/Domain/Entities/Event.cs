be04aa53a3a56ed282668104d436106f1dfe37eb
decademicorazon <zavaliathomas2@gmail.com>
2026-01-13 00:50:56 -0300

    Estructura de carpetas y arquitectura del proyecto inicial

---
+++ services/BookingService/src/Domain/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Event
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public ICollection<Seat> Seats { get; set; } = new List<Seat>();
+
+
+
+    }
+}
---
+++ services/BookingService/src/Domain/Entities/Seat.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Seat
+    {
+        public Guid Id { get; set; }
+        public string Section { get; set; }=string.Empty;
+
+        public string Number { get; set; }=string.Empty;
+
+        public decimal Price { get; set; }
+
+        public Guid EventId { get; set; }
+        public Event? Event {  get; set; }
+
+        public SeatStatus Status { get; set; }
+        public Guid UserId { get; set; }
+
+        public uint RowVersion { get; set; }
+    }
+}
